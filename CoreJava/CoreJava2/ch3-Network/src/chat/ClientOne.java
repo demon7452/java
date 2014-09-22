e1cda7571aa60ff8f1566c2b92f13d763a5e03ec
demon7452 <demonli7452@gmail.com>
2014-09-22 17:15:31 +0800

    聊天软件……未完成！

---
+++ CoreJava/CoreJava2/ch3-Network/src/chat/ClientOne.java
@@ -0,0 +1,11 @@
+package chat;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClientOne
+{
+	private ServerSocket serverSocket;
+	private Socket socketClient;
+	private Socket socketMine;
+}
