1a268f7317331203c27239c3cf9e02c50f66785e
demon7452 <demonli7452@gmail.com>
2014-09-29 16:00:31 +0800

    ch2-2.1.6-release
    put,get,limit,position
    hasRemaining

--- Java_NIO/ch2-Buffer/src/io/github/demon7452/BufferBasics/BufferBasics.java
+++ Java_NIO/ch2-Buffer/src/io/github/demon7452/BufferBasics/BufferBasics.java
@@ -2,15 +2,39 @@ package io.github.demon7452.BufferBasics;
 
 import java.nio.ByteBuffer;
 
-
-public class BufferBasics {
-
+public class BufferBasics 
+{
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		ByteBuffer buffer = ByteBuffer.allocate(10);
-		buffer.put((byte)'H').put((byte)'e').put((byte)'l').put((byte)'l').put((byte)'o');
-		
+		buffer.put((byte) 'H').put((byte) 'e').put((byte) 'l').put((byte) 'l')
+				.put((byte) 'o');
+		buffer.put((byte)'W');
+		buffer.put((byte)'o');
+		buffer.put((byte)'r');
+		buffer.put((byte)'l');
+		buffer.put((byte)'d');
+//		buffer.put((byte)'d');//溢出
+//		buffer.limit(buffer.position());//将上界属性值limit设为当前属性值position,即缓冲区有效内容的末端
+//		buffer.position(0);//将当前属性值position设为0;
+		buffer.flip();//缓冲区翻转，相当于buffer.limit(buffer.position()).position(0);等价于上面两句
+		while(buffer.hasRemaining())
+		{
+			char charByte = (char)buffer.get();
+			System.out.print(charByte);
+		}
+		System.out.println("\n"+(char)buffer.get(5));
+//		buffer.put(5,((byte)'k'));//替换
+//		System.out.println((char)buffer.get(5));
+		buffer.position(0);		
+		buffer.limit(5);
+		while(buffer.hasRemaining())
+		{
+			char charByte = (char)buffer.get();
+			System.out.print(charByte);
+		}
+
 	}
 
 }
