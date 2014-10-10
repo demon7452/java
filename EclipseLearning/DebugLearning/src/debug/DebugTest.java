638bd6f5df58a0d50e42d5a50bf57345d991a942
demon7452 <demonli7452@gmail.com>
2014-10-10 14:08:55 +0800

    debug? confusion!

---
+++ EclipseLearning/DebugLearning/src/debug/DebugTest.java
@@ -0,0 +1,22 @@
+package debug;
+
+public class DebugTest
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int number = 0;
+		number = number +1;
+		System.out.println(number);
+		try
+		{
+			
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+	}
+
+}
