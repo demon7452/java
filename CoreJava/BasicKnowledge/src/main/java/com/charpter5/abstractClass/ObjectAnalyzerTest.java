1e10ca451c55e8178decb3852d4c3183929a6b3a
lixiong <lixiong@lifang.com>
2016-04-14 16:50:19 +0800

    chapter 5 end

---
+++ CoreJava/BasicKnowledge/src/main/java/com/charpter5/abstractClass/ObjectAnalyzerTest.java
@@ -0,0 +1,76 @@
+package com.charpter5.abstractClass;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
+/**
+ * 在运行时使用反射分析对象
+ * @author demon7452
+ *
+ */
+public class ObjectAnalyzerTest {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee("demon");
+		employee.setSex("man");
+		System.out.println(new ObjectAnalyzer().toString(employee));
+	}
+	private static class ObjectAnalyzer{
+		private ArrayList<Object> visited = new ArrayList<Object>();
+		public String toString(Object object){
+			if(null == object)
+				return "null";
+			if(visited.contains(object))
+				return "...";
+			visited.add(object);
+			Class class1 = object.getClass();
+			if(String.class == class1)
+				return (String)object;
+			if(class1.isArray()){
+				String resultString = class1.getComponentType()+"[]{";
+				for(int i=0; i< Array.getLength(object);i++){
+					if(i>0)
+						resultString += ",";
+					Object val = Array.get(object, i);
+					if(class1.getComponentType().isPrimitive())
+						resultString += val;
+					else
+						resultString += toString(val);
+				}
+				return resultString + "}";
+			}
+			
+			String resultString = class1.getName();
+			do {
+				
+				Field[] fields = class1.getDeclaredFields();
+				class1 = class1.getSuperclass();
+				if(fields.length == 0)
+					continue;
+				resultString += "[";
+				AccessibleObject.setAccessible(fields, true);
+				for(Field field : fields){
+					if(!Modifier.isStatic(field.getModifiers())){
+						if(!resultString.endsWith("["))
+							resultString += ",";
+						resultString += field.getName() + "=";
+						try {
+							Object value = field.get(object);
+							if(field.getType().isPrimitive())
+								resultString += value;
+							else {
+								resultString += toString(value);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				resultString += "]";
+			} while (null != class1);
+			return resultString;
+		}
+	}
+}
