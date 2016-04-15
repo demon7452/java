7780d447a07d407069a8703186c682ea53760f04
demon7452 <demonli7452@gmail.com>
2016-04-15 14:16:00 +0800

    clone test

--- CoreJava/BasicKnowledge/src/main/java/com/charpter6/interfaceAndInnerClass/CloneTest.java
+++ CoreJava/BasicKnowledge/src/main/java/com/charpter6/interfaceAndInnerClass/CloneTest.java
@@ -1,19 +1,33 @@
 package com.charpter6.interfaceAndInnerClass;
 import java.util.Date;
-import java.util.GregorianCalendar;
 public class CloneTest {
 
 	public static void main(String[] args) {
+		//浅拷贝测试
 		Date date = new Date();
-		NoClone noA = new NoClone("noA",date);
-		System.out.println("name:"+noA.getName()+"  calendar:"+noA.getDate().getTime());
-		
+		Description description = new Description("this is a clone test!");
+		NoClone noA = new NoClone("noA",date,description);
+		System.out.println("name:"+noA.getName()+"  calendar:"+noA.getDate().getTime() + " Description:"+noA.getDescription().getDescription());
 		try {
 			NoClone noB = noA.clone();
 			noB.setName("noB");
 			noB.setDate(new Date(10000000));
-			System.out.println("name:"+noA.getName()+"  calendar:"+noA.getDate().getTime());
-			System.out.println("name:"+noB.getName()+"  calendar:"+noB.getDate().getTime());
+			noB.getDescription().setDescription("hello world!!");
+			System.out.println("name:"+noA.getName()+"  calendar:"+noA.getDate().getTime()+" Description:"+noA.getDescription().getDescription());
+			System.out.println("name:"+noB.getName()+"  calendar:"+noB.getDate().getTime()+" Description:"+noB.getDescription().getDescription());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//深拷贝测试
+		try {
+			DeepClone deepCloneA = new DeepClone("deepA", new Description("deep clone"));
+			System.out.println("Name:"+deepCloneA.getName()+" Description:"+deepCloneA.getDescription().getDescription());
+			DeepClone deepCloneB = deepCloneA.clone();
+			deepCloneB.setName("deepB");
+			deepCloneB.getDescription().setDescription("hello hahaha");
+			System.out.println("Name:"+deepCloneA.getName()+" Description:"+deepCloneA.getDescription().getDescription());
+			System.out.println("Name:"+deepCloneB.getName()+" Description:"+deepCloneB.getDescription().getDescription());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -24,9 +38,17 @@ public class CloneTest {
 class NoClone implements Cloneable{
 	private String name;
 	private Date date;
-	public NoClone(String name,Date date){
+	private Description description;
+	public NoClone(String name,Date date,Description description){
 		this.name = name;
 		this.date = date;
+		this.description = description;
+	}
+	public Description getDescription() {
+		return description;
+	}
+	public void setDescription(Description description) {
+		this.description = description;
 	}
 	public String getName() {
 		return name;
@@ -48,12 +70,24 @@ class NoClone implements Cloneable{
 	}
 }
 
-class HaveClone implements Cloneable{
+class DeepClone implements Cloneable{
 	private String name;
-	private GregorianCalendar calendar;
-	public HaveClone(String name,GregorianCalendar calendar){
+	private Date date;
+	private Description description;
+	public DeepClone(String name,Description description){
 		this.name = name;
-		this.calendar = calendar;
+		this.description = description;
+	}
+	public DeepClone(String name,Date date,Description description){
+		this.name = name;
+		this.date = date;
+		this.description = description;
+	}
+	public Description getDescription() {
+		return description;
+	}
+	public void setDescription(Description description) {
+		this.description = description;
 	}
 	public String getName() {
 		return name;
@@ -61,19 +95,38 @@ class HaveClone implements Cloneable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	public GregorianCalendar getCalendar() {
-		return calendar;
+	public Date getDate() {
+		return date;
 	}
-	public void setCalendar(GregorianCalendar calendar) {
-		this.calendar = calendar;
+	public void setDate(Date date) {
+		this.date = date;
 	}
 	
 	/**
+	 * 
 	 * 深拷贝
 	 */
-	public HaveClone clone() throws CloneNotSupportedException{
-		HaveClone cloned = (HaveClone)super.clone();
-		cloned.calendar = (GregorianCalendar)calendar.clone();
+	public DeepClone clone() throws CloneNotSupportedException{
+		DeepClone cloned = (DeepClone)super.clone();
+//		cloned.date = (Date)date.clone();
+		cloned.description = (Description)description.clone();
 		return cloned;
+	}
+}
+
+class Description implements Cloneable{
+	private String description;
+	public Description(String description){
+		this.description = description;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Description clone() throws CloneNotSupportedException{
+		return (Description)super.clone();
 	}
 }
