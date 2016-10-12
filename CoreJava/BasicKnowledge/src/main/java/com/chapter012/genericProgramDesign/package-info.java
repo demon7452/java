f208ecda503684d48a7de0a8e30ddaeae38168a2
demon7452 <demonli7452@gmail.com>
2016-10-12 14:46:04 +0800

    generic program design

---
+++ CoreJava/BasicKnowledge/src/main/java/com/chapter012/genericProgramDesign/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Function： TODO 
+ *
+ *   ver     date      	author
+ * ──────────────────────────────────
+ *   		2016年10月12日     lixiong
+ *
+ * Copyright (c) 2016, WKZF All Rights Reserved.
+*/
+
+/**
+ * Function: Generic Program Design
+ * 泛型程序设计
+ * @author   lixiong
+ * @Date	 2016年10月12日		下午2:28:20
+ *
+ * @see 	  
+ */
+
+package com.chapter012.genericProgramDesign;
